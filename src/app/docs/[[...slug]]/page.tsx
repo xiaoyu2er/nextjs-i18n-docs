f7e610f03da33ad25431807b65c125fa06675a86
yanqi.zong <yzong@turo.com>
2025-05-09 16:14:17 -0700

    feat: support AppOnly & PagesOnly

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,21 +1,32 @@
-import { source } from '@/lib/source';
+import { source } from "@/lib/source";
 import {
   DocsPage,
   DocsBody,
   DocsDescription,
   DocsTitle,
-} from 'fumadocs-ui/page';
-import { notFound } from 'next/navigation';
-import { createRelativeLink } from 'fumadocs-ui/mdx';
-import { getMDXComponents } from '@/mdx-components';
+} from "fumadocs-ui/page";
+import { notFound } from "next/navigation";
+import { createRelativeLink } from "fumadocs-ui/mdx";
+import { getMDXComponents } from "@/mdx-components";
+import { Identity } from "@/mdx/Identity";
+import { Void } from "@/mdx/Void";
+
+export function getPage(slug?: string[]) {
+  const pages = source.getPages();
+  const url = `/docs/${(slug || []).join("/")}`;
+  const page = pages.find((page) => page.url === url);
+  return page;
+}
 
 export default async function Page(props: {
   params: Promise<{ slug?: string[] }>;
 }) {
   const params = await props.params;
-  const page = source.getPage(params.slug);
+  const slug = params.slug || [];
+  const page = getPage(slug);
   if (!page) notFound();
-
+  const isAppDocs = slug[0] === "app";
+  const isPagesDocs = slug[0] === "pages";
   const MDXContent = page.data.body;
 
   return (
@@ -27,6 +38,8 @@ export default async function Page(props: {
           components={getMDXComponents({
             // this allows you to link to other pages with relative file paths
             a: createRelativeLink(source, page),
+            AppOnly: isAppDocs ? Identity : Void,
+            PagesOnly: isPagesDocs ? Identity : Void,
           })}
         />
       </DocsBody>
@@ -42,7 +55,7 @@ export async function generateMetadata(props: {
   params: Promise<{ slug?: string[] }>;
 }) {
   const params = await props.params;
-  const page = source.getPage(params.slug);
+  const page = getPage(params.slug);
   if (!page) notFound();
 
   return {
