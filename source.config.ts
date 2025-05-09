9f0143174cec2a0eb90b85c2c8bf6df0b9bb0223
yanqi.zong <yzong@turo.com>
2025-05-08 20:31:51 -0700

    feat: add DOCS_DIR env

--- source.config.ts
+++ source.config.ts
@@ -3,11 +3,18 @@ import {
   defineDocs,
   frontmatterSchema,
   metaSchema,
-} from 'fumadocs-mdx/config';
+} from "fumadocs-mdx/config";
 
+if (!process.env.DOCS_DIR) {
+  throw new Error(
+    "DOCS_DIR is not set. Please set it in your environment variables."
+  );
+}
+console.log("DOCS_DIR:", process.env.DOCS_DIR);
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
+  dir: process.env.DOCS_DIR,
   docs: {
     schema: frontmatterSchema,
   },
