4e6b9a6f36e71f067b8598b8f1e1529e54872dff
yanqi.zong <yzong@turo.com>
2025-05-08 21:31:16 -0700

    feat: locale?.toLowerCase

--- source.config.ts
+++ source.config.ts
@@ -9,7 +9,7 @@ console.log("process.env.LANGUAGE_CODE", process.env.LANGUAGE_CODE);
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
-  dir: `content/${process.env.LANGUAGE_CODE}/docs`,
+  dir: `content/${process.env.LANGUAGE_CODE?.toLowerCase()}/docs`,
   docs: {
     schema: frontmatterSchema,
   },
