f92efc1be8670c11a33d0546cd6d089e2f39ac33
yanqi.zong <yzong@turo.com>
2025-05-09 16:13:34 -0700

    feat: update getMDXComponents

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,16 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import defaultMdxComponents from "fumadocs-ui/mdx";
+
+import { Image } from "./mdx/Image";
+import { Check, Cross } from "./mdx/Check";
+import type { MDXComponents } from "mdx/types";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Image,
+    Cross,
+    Check,
     ...components,
   };
 }
