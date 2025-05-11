f29eb1f50a3a5ecf9ae93b8af865d88f97013034
yanqi.zong <yzong@turo.com>
2025-05-10 17:50:29 -0700

    feat: add tabs icon

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,17 +1,27 @@
-import { docs } from '@/.source';
-import { loader } from 'fumadocs-core/source';
+import { docs } from "@/.source";
+import * as icons from "@/mdx/Icon";
+import { loader } from "fumadocs-core/source";
+import { createElement } from "react";
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
-  baseUrl: '/docs',
+  baseUrl: "/docs",
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (icon && icon in icons)
+      return createElement(icons[icon as keyof typeof icons]);
+  },
   url(slugs) {
     // remove \d\d- from slug
-    const url =  '/docs/' + slugs.map((slug) => {
-      return slug.replace(/^\d\d-/, '');
-    }).join('/');
-    
+    const url =
+      "/docs/" +
+      slugs
+        .map((slug) => {
+          return slug.replace(/^\d\d-/, "");
+        })
+        .join("/");
+
     return url;
   },
   // https://fumadocs.dev/docs/headless/source-api#page-tree-1
@@ -21,5 +31,4 @@ export const source = loader({
       return node;
     },
   },
-
 });
---
+++ src/mdx/Icon.tsx
@@ -0,0 +1,35 @@
+export const NextAppIcon = () => (
+  <svg
+    data-testid="geist-icon"
+    height="16"
+    strokeLinejoin="round"
+    style={{ color: "currentColor" }}
+    viewBox="0 0 16 16"
+    width="16"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 0.154663L8.34601 0.334591L14.596 3.58459L15 3.79466V4.25V11.75V12.2053L14.596 12.4154L8.34601 15.6654L8 15.8453L7.65399 15.6654L1.40399 12.4154L1 12.2053V11.75V4.25V3.79466L1.40399 3.58459L7.65399 0.334591L8 0.154663ZM2.5 11.2947V5.44058L7.25 7.81559V13.7647L2.5 11.2947ZM8.75 13.7647L13.5 11.2947V5.44056L8.75 7.81556V13.7647ZM8 1.84534L12.5766 4.22519L7.99998 6.51352L3.42335 4.2252L8 1.84534Z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const NextPagesIcon = () => (
+  <svg
+    data-testid="geist-icon"
+    height="16"
+    strokeLinejoin="round"
+    viewBox="0 0 16 16"
+    width="16"
+    style={{ color: "currentColor" }}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M14.5 6.5V13.5C14.5 14.8807 13.3807 16 12 16H4C2.61929 16 1.5 14.8807 1.5 13.5V1.5V0H3H8H9.08579C9.351 0 9.60536 0.105357 9.79289 0.292893L14.2071 4.70711C14.3946 4.89464 14.5 5.149 14.5 5.41421V6.5ZM13 6.5V13.5C13 14.0523 12.5523 14.5 12 14.5H4C3.44772 14.5 3 14.0523 3 13.5V1.5H8V5V6.5H9.5H13ZM9.5 2.12132V5H12.3787L9.5 2.12132Z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
