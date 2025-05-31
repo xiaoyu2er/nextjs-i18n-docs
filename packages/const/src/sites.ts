47e9496bc4e9f7a8b55b592943d9b493d460557b
yanqi.zong <yzong@turo.com>
2025-05-30 20:21:35 -0700

    feat: add pending sites

--- packages/const/src/sites.ts
+++ packages/const/src/sites.ts
@@ -5,3 +5,12 @@ export const SITES: Record<Locale, string> = {
   'zh-hans': 'https://zh-hans.nextjs.im',
   'zh-hant': 'https://zh-hant.nextjs.im',
 };
+
+export const PENDING_SITES: Record<Locale, string> = {
+  ja: 'https://ja.nextjs.im',
+  es: 'https://es.nextjs.im',
+  de: 'https://de.nextjs.im',
+  fr: 'https://fr.nextjs.im',
+  ru: 'https://ru.nextjs.im',
+  ar: 'https://ar.nextjs.im',
+};
