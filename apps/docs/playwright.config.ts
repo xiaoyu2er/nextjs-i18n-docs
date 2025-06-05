47d7efb576b1af2cb7ac95d88596147939b4349f
yanqi.zong <yzong@turo.com>
2025-06-05 16:31:55 -0700

    feat: update playwright config

--- apps/docs/playwright.config.ts
+++ apps/docs/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:7878',
     trace: 'on-first-retry',
   },
   projects: [
@@ -18,8 +18,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: 'pnpm dev',
-    url: 'http://localhost:3000',
+    command: 'pnpm dev --port 7878',
+    url: 'http://localhost:7878',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
