bfbdf5686a8a80c292bfdfc79942b485457d2d4b
yanqi.zong <yzong@turo.com>
2025-06-08 13:48:40 -0700

    feat: update sitemap

--- apps/docs/src/app/sitemap.ts
+++ apps/docs/src/app/sitemap.ts
@@ -11,19 +11,15 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: url('/'),
       changeFrequency: 'monthly',
-      priority: 1,
     },
 
     {
       url: url('/blog'),
       changeFrequency: 'monthly',
-      priority: 0.8,
     },
 
     {
       url: url('/learn'),
-      changeFrequency: 'monthly',
-      priority: 0.8,
     },
 
     ...(await Promise.all(
@@ -41,12 +37,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       ]
         .flat()
         .map(async (page) => {
-          const { 'translation-updated-at': lastModified } = page.data;
           return {
             url: url(page.url),
-            lastModified: lastModified ? new Date(lastModified) : undefined,
-            changeFrequency: 'weekly',
-            priority: 0.5,
           } as MetadataRoute.Sitemap[number];
         }),
     )),
